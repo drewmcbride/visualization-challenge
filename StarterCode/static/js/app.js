7323e19acbb21c46d88802c0eee8add57c7b211b
DrewMcB <drew.mcbride89@gmail.com>
2021-04-05 22:09:07 -0500

    Cleaned up code and reworked chart function

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,15 +1,3 @@
-// d3.json("./samples.json").then((importedData) => {
-//   // console.log(importedData);
-//   var data = importedData;
-
-//   var names = data.names;
-  // var metadata = data.metadata;
-  // var samples = data.samples;
-
-//   // console.log(samples);
-
-// });
-
 function init() {
   d3.json("./samples.json").then((importedData) => {
     var names = importedData.names;
@@ -23,59 +11,67 @@ function init() {
       .property("value", sample);
     })
     
-    display.on("change", updateMetadata)
+    // display.on("change", updateMetadata)
     // display.on("change", buildChart)
 
-    var firstSample = names[0];
+    // var firstSample = names[0];
 
-    var displayPanel = d3.select("#sample-metadata");
-    // console.log(metaData)
+    // var displayPanel = d3.select("#sample-metadata");
+    // // console.log(metaData)
     sample = d3.select("#selDataset").property("value")
-    // console.log(sample)
+    updateMetadata(sample)
+    updateChart(sample)
+    // // console.log(sample)
     
-    var filteredData = metaData.filter(row => row.id == firstSample)[0];
-    Object.entries(filteredData).forEach(([key, value]) => {
-      displayPanel.append("h6")
-      .text(`${key}: ${value}`);
-    });
+    // var filteredData = metaData.filter(row => row.id == firstSample)[0];
+    // Object.entries(filteredData).forEach(([key, value]) => {
+    //   displayPanel.append("h6")
+    //   .text(`${key}: ${value}`);
+    // });
 
-    var displayBar = d3.select("#bar");
-    var otu_Ids = samples[0]["otu_ids"].slice(0,10).map(id => "otu"+String(id)).reverse()
-    var sample_Values = samples[0]["sample_values"].slice(0,10).reverse()
-    var otu_Labels = samples[0]["otu_Labels"]
+    // var otu_Ids = samples[0]["otu_ids"].slice(0,10).map(id => "otu"+String(id)).reverse()
+    // var sample_Values = samples[0]["sample_values"].slice(0,10).reverse()
+    // var otu_Labels = samples[0]["otu_Labels"]
 
-    // console.log(sample_Values)
+    // // console.log(sample_Values)
     
-    var trace1 = {
-      type: "bar",
-      orientation: "h",
-      // mode: "lines",
-      // name: otu_Ids,
-      x: sample_Values,
-      y: otu_Ids,
-      // line: {
-      //   color: "#17BECF"
-      // }
-    };
-    Plotly.newPlot("bar", [trace1]);
+    // var trace1 = {
+    //   type: "bar",
+    //   orientation: "h",
+    //   // mode: "lines",
+    //   // name: otu_Ids,
+    //   x: sample_Values,
+    //   y: otu_Ids,
+    //   // line: {
+    //   //   color: "#17BECF"
+    //   // }
+    // };
+    // Plotly.newPlot("bar", [trace1]);
   });
 }; 
 
     // buildChart(firstSample);
 
-function updateChart() {
+
+function optionChanged(value) {
+  console.log(value)
+  updateMetadata(value)
+  updateChart(value)
+};
+
+function updateChart(sample) {
   d3.json("./samples.json").then((importedData) => {
     var displayBar = d3.select("#bar");
-    sample = d3.select("#selDataset").property("value")
+    // sample = d3.select("#selDataset").property("value")
 
     var samples = importedData.samples
     var filteredSamples = samples.filter(row => row.id == sample)[0];
 
     console.log(filteredSamples)
 
-    var otu_Ids = filteredSamples[0]["otu_ids"].slice(0,10).map(id => "otu"+String(id)).reverse()
-    var sample_Values = filteredSamples[0]["sample_values"].slice(0,10).reverse()
-    var otu_Labels = filteredSamples[0]["otu_Labels"]
+    var otu_Ids = filteredSamples["otu_ids"].slice(0,10).map(id => "otu"+String(id)).reverse()
+    var sample_Values = filteredSamples["sample_values"].slice(0,10).reverse()
+    var otu_Labels = filteredSamples["otu_Labels"]
 
     // console.log(sample_Values)
     
@@ -96,15 +92,16 @@ function updateChart() {
 
 
 
-function updateMetadata() {
+function updateMetadata(sample) {
   d3.json("./samples.json").then((importedData) => {
     var displayPanel = d3.select("#sample-metadata");
     displayPanel.html("");
     var metaData = importedData.metadata;
     // console.log(metaData)
-    sample = d3.select("#selDataset").property("value")
-    // console.log(sample)
+    // sample = d3.select("#selDataset").property("value")
+    console.log("sample",sample)
     
+  
     var filteredData = metaData.filter(row => row.id == sample)[0];
     Object.entries(filteredData).forEach(([key, value]) => {
       displayPanel.append("h6")
@@ -113,34 +110,6 @@ function updateMetadata() {
   })
 };
 
-// On change to the DOM, call getData()
-// d3.selectAll("#selDataset").on("change", getData);
-
-// // Function called by DOM changes
-// function getData() {
-//   var dropdownMenu = d3.select("#selDataset");
-//   // Assign the value of the dropdown menu option to a variable
-//   var dataset = dropdownMenu.property("value");
-//   // Initialize an empty array for the country's data
-//   var data = [];
-
-//   if (dataset == 'us') {
-//       data = us;
-//   }
-//   else if (dataset == 'uk') {
-//       data = uk;
-//   }
-//   else if (dataset == 'canada') {
-//       data = canada;
-//   }
-//   // Call function to update the chart
-//   updatePlotly(data);
-// }
-
-// // Update the restyled plot's values
-// function updatePlotly(newdata) {
-//   Plotly.restyle("bar", "values", [newdata]);
-
   
 init();
-// metaData_table();
+
