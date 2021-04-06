c22aade58d1990415bd7b5fd6089859564b70efd
DrewMcB <drew.mcbride89@gmail.com>
2021-04-05 23:07:46 -0500

    Final edits to get filters to get selection filters to work.

--- app.js
+++ app.js
@@ -11,70 +11,32 @@ function init() {
       .property("value", sample);
     })
     
-    // display.on("change", updateMetadata)
-    // display.on("change", buildChart)
-
-    // var firstSample = names[0];
-
-    // var displayPanel = d3.select("#sample-metadata");
-    // // console.log(metaData)
     sample = d3.select("#selDataset").property("value")
     updateMetadata(sample)
     updateChart(sample)
     updateBubble(sample)
-    // // console.log(sample)
     
-    // var filteredData = metaData.filter(row => row.id == firstSample)[0];
-    // Object.entries(filteredData).forEach(([key, value]) => {
-    //   displayPanel.append("h6")
-    //   .text(`${key}: ${value}`);
-    // });
-
-    // var otu_Ids = samples[0]["otu_ids"].slice(0,10).map(id => "otu"+String(id)).reverse()
-    // var sample_Values = samples[0]["sample_values"].slice(0,10).reverse()
-    // var otu_Labels = samples[0]["otu_Labels"]
-
-    // // console.log(sample_Values)
-    
-    // var trace1 = {
-    //   type: "bar",
-    //   orientation: "h",
-    //   // mode: "lines",
-    //   // name: otu_Ids,
-    //   x: sample_Values,
-    //   y: otu_Ids,
-    //   // line: {
-    //   //   color: "#17BECF"
-    //   // }
-    // };
-    // Plotly.newPlot("bar", [trace1]);
   });
 }; 
 
-    // buildChart(firstSample);
 
 
 function optionChanged(value) {
-  console.log(value)
   updateMetadata(value)
   updateChart(value)
+  updateBubble(value)
 };
 
 function updateChart(sample) {
   d3.json("./samples.json").then((importedData) => {
     var displayBar = d3.select("#bar");
-    // sample = d3.select("#selDataset").property("value")
 
     var samples = importedData.samples
     var filteredSamples = samples.filter(row => row.id == sample)[0];
 
-    // console.log(filteredSamples)
-
     var otu_Ids = filteredSamples["otu_ids"].slice(0,10).map(id => "otu"+String(id)).reverse()
     var sample_Values = filteredSamples["sample_values"].slice(0,10).reverse()
     var otu_Labels = filteredSamples["otu_labels"]
-
-    // console.log(otu_Labels)
     
     var trace1 = {
       type: "bar",
@@ -123,7 +85,7 @@ function updateBubble(sample) {
     var sample_Values = filteredSamples["sample_values"]
     var otu_Labels = filteredSamples["otu_labels"]
 
-    console.log(otu_Labels)
+    console.log(otu_Ids)
     
     var trace1 = {
       type: "scatter",
